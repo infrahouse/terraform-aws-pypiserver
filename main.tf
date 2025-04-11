0057baf1bf26a963c3fb804ac4c79970a1365d33
Maksym Kryva <maksym@tinyfish.io>
2025-04-11 10:22:21 +0300

    Merge pull request #16 from infrahouse/extend_var_interface
    add extra cmds for ECS cloud init and extra policy for instance profile

--- main.tf
+++ main.tf
@@ -3,25 +3,29 @@ locals {
 }
 module "pypiserver" {
   source  = "registry.infrahouse.com/infrahouse/ecs/aws"
-  version = "5.3.0"
+  version = "5.7.1"
   providers = {
     aws     = aws
     aws.dns = aws.dns
   }
-  healthcheck_path              = "/"
-  environment                   = var.environment
-  asg_subnets                   = var.asg_subnets
-  asg_min_size                  = 1
-  asg_max_size                  = 1
-  ami_id                        = var.ami_id
-  asg_instance_type             = var.asg_instance_type
-  dns_names                     = var.dns_names
-  docker_image                  = "pypiserver/pypiserver:latest"
-  internet_gateway_id           = var.internet_gateway_id
-  load_balancer_subnets         = var.load_balancer_subnets
-  service_name                  = "pypiserver"
-  ssh_key_name                  = var.ssh_key_name
-  zone_id                       = var.zone_id
+  healthcheck_path         = "/"
+  environment              = var.environment
+  asg_subnets              = var.asg_subnets
+  asg_min_size             = 1
+  asg_max_size             = 1
+  ami_id                   = var.ami_id
+  asg_instance_type        = var.asg_instance_type
+  dns_names                = var.dns_names
+  docker_image             = "pypiserver/pypiserver:latest"
+  internet_gateway_id      = var.internet_gateway_id
+  load_balancer_subnets    = var.load_balancer_subnets
+  service_name             = "pypiserver"
+  ssh_key_name             = var.ssh_key_name
+  zone_id                  = var.zone_id
+  cloudinit_extra_commands = var.cloudinit_extra_commands
+
+  extra_instance_profile_permissions = var.extra_instance_profile_permissions
+
   container_healthcheck_command = "/usr/local/bin/python -c \"import socket; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.connect(('127.0.0.1', ${local.container_port}))\" || exit 1"
   container_command = [
     "run", "-p", local.container_port, "--server", "gunicorn", "--authenticate", "download,list,update", "-P", "/data/.htpasswd"
